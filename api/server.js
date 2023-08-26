ef1fb7b3aa938718102f4802d0ce7d4e4ab7f3ba
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-08-25 19:08:19 -0500

    server and initial config done!

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//api test
+app.get('/', (req,res)=>{
+    res.json({
+        message: "API - My own api test"
+    })
+})
+
+
+const PORT = process.env.PORT
+//listen
+app.listen(PORT,()=>{
+    console.log(`Server running on port: ${PORT}`)
+})
+
